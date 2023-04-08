ae4060cf380db2282fb45a8a74b72010e9fc63f6
qinterfly <qinterfly@gmail.com>
2023-04-08 15:02:38 +0700

    v1.0.1. Rescale all the controls

--- src/gui/models/AbstractGraphModel.cs
+++ src/gui/models/AbstractGraphModel.cs
@@ -18,9 +18,10 @@ namespace GenCalc.Gui.Plot
             mGraph.plt.AntiAlias(true, true, true);
             mGraph.plt.Grid(lineStyle: LineStyle.Dot);
             mGraph.Configure(lowQualityWhileDragging: false, lowQualityOnScrollWheel: false);
-            mGraph.plt.XLabel(fontSize: mkFontSize);
-            mGraph.plt.YLabel(fontSize: mkFontSize);
-            mGraph.plt.Ticks(numericFormatStringX: "G9", numericFormatStringY: "G9");
+            mGraph.plt.XLabel(fontSize: mkLabelFontSize);
+            mGraph.plt.YLabel(fontSize: mkLabelFontSize);
+            mGraph.plt.Ticks(numericFormatStringX: "G9", numericFormatStringY: "G9", fontSize: mkTickFontSize);
+            mGraph.plt.Legend(fontSize: mkLegendFontSize);
             graph.MouseDown += selectPoint;
         }
         
@@ -110,7 +111,9 @@ namespace GenCalc.Gui.Plot
         protected WpfPlot mGraph = null;
         protected const int mkLineWidth = 2;
         protected const int mkMarkerSize = 5;
-        protected const int mkFontSize = 14;
+        protected const int mkLabelFontSize = 16;
+        protected const int mkLegendFontSize = 16;
+        protected const int mkTickFontSize = 15;
         // Properties
         public WpfPlot Graph { get { return mGraph; } }
     }
